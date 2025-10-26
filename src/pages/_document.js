5dc4fd08da0097f04be4a1247e47383dc83d26f6
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-10-26 22:04:06 +0000

    feat: main

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,13 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Navbar />
       <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
-      <Footer />
     </Html>
   );
 }
